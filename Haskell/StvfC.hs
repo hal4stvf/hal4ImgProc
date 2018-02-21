753a47fce480e22495379f0fd30e9f81e29325d1
schmeckpeper <hal4stuvf@gmail.com>
2018-02-21 23:04:18 +0100

    hinzugefügt. Jetzt nicht klar was passiert ist?

--- Haskell/StvfC.hs
+++ Haskell/StvfC.hs
@@ -1,12 +1,19 @@
 module StvfC where
 
+import Foreign
 import Foreign.C.Types
+-- 
+-- fibonacci :: Int -> Int
+-- fibonacci n = fibs !! n
+--   where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+-- 
+-- fibonacci_hs :: CInt -> CInt
+-- fibonacci_hs = fromIntegral . fibonacci . fromIntegral 
+-- 
+-- foreign export ccall fibonacci_hs :: CInt -> CInt
 
-fibonacci :: Int -> Int
-fibonacci n = fibs !! n
-  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+  zeros :: [[Int]] -> [[Int]]
+  zeros xss = [[0 | x <-xs] | xs <- xss]
+  
+  foreign export ccall zeros_hs :: [[CInt]] -> [[CInt]]
 
-fibonacci_hs :: CInt -> CInt
-fibonacci_hs = fromIntegral . fibonacci . fromIntegral 
-
-foreign export ccall fibonacci_hs :: CInt -> CInt
